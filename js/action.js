466d12e13d9fe46df9e78594938e59fd9d837c66
Fabio Petrone <fa610movic90@gmail.com>
2020-12-10 15:07:59 +0100

    prove

--- js/action.js
+++ js/action.js
@@ -11,3 +11,19 @@ var titoli=document.getElementsByTagName('h1')[1];
 console.log(titoli);
 
 // outerText su chrome va su firefox no
+
+
+// for (var i=0;i<50;i++){
+// if(i==30) {continue;}
+//
+// document.write("sto al giro"+i+"<br>");
+// }
+
+ // break spezza
+ var div=document.getElementById('lollo');
+ var testo="";
+for (var i = 0; i < 5; i++) {
+testo+="<h1>ciao come va</h1>";}
+div.innerHTML=testo;
+
+}
